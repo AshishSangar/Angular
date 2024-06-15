dbac09614ad40ffff68c23ff295d2a29afcd731d
AshishSangar <ashishsangar2398@gmail.com>
2024-06-15 10:43:39 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'meal-db-example';
+  categories: any[] = [];
+  url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+      this.fetchMeals();
+  }
+
+  fetchMeals() {
+    this.http.get(this.url).subscribe((response: any) => {
+      this.categories = response.categories;
+    });    
+  }
 }
